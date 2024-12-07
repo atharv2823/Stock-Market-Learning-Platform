6592df4279f444970640f48d79f23200bd2130a0
athav2823 <atharvaneware@gmail.com>
2024-12-07 20:04:23 +0530

    updated

--- client/src/Login/Login.js
+++ client/src/Login/Login.js
@@ -3,7 +3,7 @@ import "./Login.css"
 import { Link } from 'react-router-dom'
 
 import axios from 'axios'
-import toast from 'react-hot-toast'
+import toast, { Toaster } from 'react-hot-toast'
 import {useState} from 'react'
 
 
@@ -70,7 +70,10 @@ function Login() {
       </div>
     </form>
   </div>
+  <Toaster/>
 </div>
+
+
 
   )
 }
